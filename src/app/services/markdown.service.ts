d514d7545526ea00fb90fffef2b67dc5f31e1734
Dao Khoi Nguyen <nguyendk.on@gmail.com>
2023-07-21 15:59:44 +0700

    fix: fixed warning marked

--- src/app/services/markdown.service.ts
+++ src/app/services/markdown.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { marked } from 'marked';
+import { gfmHeadingId } from 'marked-gfm-heading-id';
 
 @Injectable({
     providedIn: 'root',
 })
 export class MarkdownService {
     constructor() {
+        marked.use(
+            gfmHeadingId({
+                prefix: 'app',
+            })
+        );
         marked.setOptions({});
     }
 
     convertToHtml(markdownText: string): string {
-        return marked(markdownText);
+        return marked(markdownText, { mangle: false });
     }
 }
